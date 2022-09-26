76ed6932523cad5a0e1cbc155335f73def999e71
SaadHazrawi <113783122+SaadHazrawi@users.noreply.github.com>
2022-09-26 10:48:14 +0300

    Add files via upload

---
+++ BookDomin/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDomin
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public List<PrintsBook> PrintsBooks { get; set; } = new List<PrintsBook>();
+    }
+}
---
+++ BookDomin/PrintsBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDomin
+{
+    public class PrintsBook
+    {
+        public int Id { get; set; }
+        //اسم دار الطباعة
+        public string LibName { get; set; }
+        public int BookID { get; set; }
+        public Book Book { get; set; } = new Book();
+    }
+}
